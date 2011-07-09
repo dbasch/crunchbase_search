efe4b41c471111a24bf12e9d8d6cee516dc0e6d1
Diego Basch <dbasch@gmail.com>
2011-07-09 14:54:23 -0700

    cleaned up a bit

--- fetch_crunchbase.rb
+++ fetch_crunchbase.rb
@@ -1,25 +1,36 @@
 #!/usr/bin/ruby
 
+#Fetch everything from the Crunchbase API
+#warning: don't hammer the API. Set PAUSE to a reasonable value.
+
+PAUSE=0.2
+
 require 'rubygems'
 require 'indextank'
 require 'json'
 require 'open-uri'
 
 def fetch(name, uri)
+  begin
     f = File.open(name, 'w')
+    puts uri
     f.write(open(uri).read)
     f.close
+    sleep PAUSE
+  rescue
+    puts 'error'
+  end
 end
 
+categories = [ ['person', 'people'], 
+  ['company', 'companies'], 
+  ['product', 'products'], 
+  ['financial-organization', 'financial-organizations']]
 
-people = JSON.parse(File.read('data/people.js'))
-people.each do |x| 
-    begin
-        toget = 'http://api.crunchbase.com/v/1/person/' + x['permalink'] + '.js'
-        puts toget
-        fetch('data/people/' + x['permalink'] +'.js', toget)
-    rescue
-        puts 'error'
-    end
+categories.each do |c|
+  puts 'fetching ' + c[1]
+  JSON.parse(File.read('data/' + c[1] + '.js')).each do |x| 
+    toget = 'http://api.crunchbase.com/v/1/' + c[0] + '/' + x['permalink'] + '.js'
+    fetch('data/' + c[1] + '/' + x['permalink'] +'.js', toget)
+  end
 end
-
