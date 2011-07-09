9bc7f1179c23e439ce2f295fc777775f7ca577fa
Diego Basch <dbasch@gmail.com>
2011-07-09 14:54:44 -0700

    added

---
+++ getfiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p data/companies
+mkdir data/people
+mkdir data/products
+mkdir data/financial-organizations
+
+cd data
+
+#wget http://api.crunchbase.com/v/1/companies.js
+#wget http://api.crunchbase.com/v/1/people.js
+#wget http://api.crunchbase.com/v/1/products.js
+#wget http://api.crunchbase.com/v/1/financial-organizations.js
+
+cd ..
+
