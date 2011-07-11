84ad342753e7ddf7f167591a29e5e9f9280e18c6
Diego Basch <dbasch@gmail.com>
2011-07-11 12:44:55 -0700

    added service providers

--- getfiles.sh
+++ getfiles.sh
@@ -11,6 +11,7 @@ wget http://api.crunchbase.com/v/1/companies.js
 wget http://api.crunchbase.com/v/1/people.js
 wget http://api.crunchbase.com/v/1/products.js
 wget http://api.crunchbase.com/v/1/financial-organizations.js
+wget http://api.crunchbase.com/v/1/service-providers.js
 
 cd ..
 
